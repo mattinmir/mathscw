d0e13b732d8c0efee7b9af48bbac08cc41d7971b
airbusa1 <aibrahim9449@gmail.com>
2015-03-25 17:39:43 +0000

    Create HeunRL.m
    TOP SECRET
    FOR MATTIN AND AHMEDS EYES ONLY

---
+++ HeunRL.m
@@ -0,0 +1,33 @@
+function vout = huenRL(Vin , R , L ,h, io, tf)
+
+N = round(tf/h);
+
+for j = 1:N+1
+    t(j) = h*(j-1);
+end
+
+y(1) = io;
+
+for j =1:N
+    igrad = (1/L)*(Vin(j*h) - R*y(j)); %i'
+    ip = y(j) + h*(igrad); %predictor
+    ingrad = (1/L)*(Vin((j+1)*h) - R*ip); % i'+1
+    iave = 0.5*(igrad + ingrad); %average
+    vout(j+1) = Vin(j*h) - R*y(j); %output voltage
+    y(j+1) = y(j) + h*iave; %corrector
+end
+
+
+% plot(t, vout, 'b*');
+% grid;
+% xlabel('Time(s)');
+% ylabel('Voltage (V)');
+
+
+
+
+
+
+
+
+
